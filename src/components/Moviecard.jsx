64cf39b945aea4af333a13d858b3621c4d1405ce
vedanshun05 <162875850+vedanshun05@users.noreply.github.com>
2025-04-22 17:08:29 +0530

    Update MovieCard.jsx

--- src/components/Moviecard.jsx
+++ src/components/Moviecard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Moviecard = ({movie: {title, vote_average, poster_path, release_date, original_language}}) => {
+const MovieCard = ({movie: {title, vote_average, poster_path, release_date, original_language}}) => {
     return (
         <div className='movie-card'>
             <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'} alt={title}/>
@@ -23,4 +23,4 @@ const Moviecard = ({movie: {title, vote_average, poster_path, release_date, orig
     )
 }
 
-export default Moviecard
+export default MovieCard
